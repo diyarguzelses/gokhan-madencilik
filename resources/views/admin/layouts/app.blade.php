770c895fda8d36670bc5325ad246803b24e244c3
diyarguzelses <diyarguzelses@gmail.com>
2025-02-01 15:57:42 +0300

    user profile update

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -24,7 +24,7 @@
     <style>
 
     </style>
-    <meta charset="utf-8" />
+    <meta charset="utf-8"/>
     <meta
         name="viewport"
         content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
@@ -32,32 +32,33 @@
 
     <title>Çetiner Panel</title>
 
-    <meta name="description" content="" />
+    <meta name="description" content=""/>
 
 
     <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="{{asset('sneat/assets/img/favicon/favicon.ico')}}" />
+    <link rel="icon" type="image/x-icon" href="{{asset('sneat/assets/img/favicon/favicon.ico')}}"/>
 
     <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
     <link
         href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
         rel="stylesheet"
     />
 
     <!-- Icons. Uncomment required icon fonts -->
-    <link rel="stylesheet" href="{{asset('/sneat/assets/vendor/fonts/boxicons.css')}}" />
+    <link rel="stylesheet" href="{{asset('/sneat/assets/vendor/fonts/boxicons.css')}}"/>
 
     <!-- Core CSS -->
-    <link rel="stylesheet" href="{{asset('/sneat/assets/vendor/css/core.css')}}" class="template-customizer-core-css" />
-    <link rel="stylesheet" href="{{asset('/sneat/assets/vendor/css/theme-default.css')}}" class="template-customizer-theme-css" />
-    <link rel="stylesheet" href="{{asset('/sneat/assets/css/demo.css')}}" />
+    <link rel="stylesheet" href="{{asset('/sneat/assets/vendor/css/core.css')}}" class="template-customizer-core-css"/>
+    <link rel="stylesheet" href="{{asset('/sneat/assets/vendor/css/theme-default.css')}}"
+          class="template-customizer-theme-css"/>
+    <link rel="stylesheet" href="{{asset('/sneat/assets/css/demo.css')}}"/>
 
     <!-- Vendors CSS -->
-    <link rel="stylesheet" href="{{asset('/sneat/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css')}}" />
+    <link rel="stylesheet" href="{{asset('/sneat/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css')}}"/>
 
-    <link rel="stylesheet" href="{{asset('/sneat/assets/vendor/libs/apex-charts/apex-charts.css')}}" />
+    <link rel="stylesheet" href="{{asset('/sneat/assets/vendor/libs/apex-charts/apex-charts.css')}}"/>
     <link href="https://cdn.datatables.net/2.1.7/css/dataTables.dataTables.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
 
@@ -248,36 +249,22 @@
                     <!-- Search -->
                     <div class="navbar-nav align-items-center">
                         <div class="nav-item d-flex align-items-center">
-                            <i class="bx bx-search fs-4 lh-0"></i>
-                            <input
-                                type="text"
-                                class="form-control border-0 shadow-none"
-                                placeholder="Search..."
-                                aria-label="Search..."
-                            />
+
                         </div>
                     </div>
                     <!-- /Search -->
 
                     <ul class="navbar-nav flex-row align-items-center ms-auto">
                         <!-- Place this tag where you want the button to render. -->
-                        <li class="nav-item lh-1 me-3">
-                            <a
-                                class="github-button"
-                                href="https://github.com/themeselection/sneat-html-admin-template-free"
-                                data-icon="octicon-star"
-                                data-size="large"
-                                data-show-count="true"
-                                aria-label="Star themeselection/sneat-html-admin-template-free on GitHub"
-                            >Star</a
-                            >
-                        </li>
+
 
                         <!-- User -->
                         <li class="nav-item navbar-dropdown dropdown-user dropdown">
-                            <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
+                            <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);"
+                               data-bs-toggle="dropdown">
                                 <div class="avatar avatar-online">
-                                    <img src="{{asset('sneat/assets/img/avatars/1.png')}}" alt class="w-px-40 h-auto rounded-circle" />
+                                    <img src="{{asset('profile/img.png')}}" alt
+                                         class="w-px-40 h-auto rounded-circle"/>
                                 </div>
                             </a>
                             <ul class="dropdown-menu dropdown-menu-end">
@@ -286,49 +273,35 @@
                                         <div class="d-flex">
                                             <div class="flex-shrink-0 me-3">
                                                 <div class="avatar avatar-online">
-                                                    <img src="{{asset('sneat/assets/img/avatars/1.png')}}" alt class="w-px-40 h-auto rounded-circle" />
+                                                    <img src="{{asset('profile/img.png')}}" alt
+                                                         class="w-px-40 h-auto rounded-circle"/>
                                                 </div>
                                             </div>
                                             <div class="flex-grow-1">
-                                                <span class="fw-semibold d-block">John Doe</span>
-                                                <small class="text-muted">Admin</small>
+    <span class="fw-semibold d-block">
+        {{ Auth::user()->username ?? 'Misafir' }}
+    </span>
                                             </div>
+
                                         </div>
                                     </a>
                                 </li>
                                 <li>
                                     <div class="dropdown-divider"></div>
                                 </li>
-                                <li>
-                                    <a class="dropdown-item" href="#">
-                                        <i class="bx bx-user me-2"></i>
-                                        <span class="align-middle">My Profile</span>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a class="dropdown-item" href="#">
-                                        <i class="bx bx-cog me-2"></i>
-                                        <span class="align-middle">Settings</span>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a class="dropdown-item" href="#">
-                        <span class="d-flex align-items-center align-middle">
-                          <i class="flex-shrink-0 bx bx-credit-card me-2"></i>
-                          <span class="flex-grow-1 align-middle">Billing</span>
-                          <span class="flex-shrink-0 badge badge-center rounded-pill bg-danger w-px-20 h-px-20">4</span>
-                        </span>
-                                    </a>
-                                </li>
-                                <li>
-                                    <div class="dropdown-divider"></div>
-                                </li>
+
+
                                 <li>
                                     <form action="{{ route('logout') }}" method="POST">
                                         @csrf
-                                        <button type="submit" class="btn btn-danger">Logout</button>
+                                        <button type="submit" class="dropdown-item d-flex align-items-center">
+                                            <i class="bx bx-log-out me-2"></i>
+                                            <span class="align-middle">Çıkış Yap</span>
+                                        </button>
                                     </form>
                                 </li>
+
+
                             </ul>
                         </li>
                         <!--/ User -->
@@ -373,7 +346,6 @@
     <div class="layout-overlay layout-menu-toggle"></div>
 </div>
 <!-- / Layout wrapper -->
-
 
 
 <!-- Core JS -->
