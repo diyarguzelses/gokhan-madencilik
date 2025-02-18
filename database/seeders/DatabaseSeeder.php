2b16415241d785573667d6a2f684d549c28bd57a
diyarguzelses <diyarguzelses@gmail.com>
2025-02-18 19:38:58 +0300

    seeder fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([UserSeeder::class]);
-        $this->call([SettingsSeeder::class]);
-        $this->call([DummyDataSeeder::class]);
+
 
     }
 }
